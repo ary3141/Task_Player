413151f2220814ed5d665d35cfb1b9264c59da71
Muhammad Dwiva Arya Erlangga <121459986+ary3141@users.noreply.github.com>
2024-11-14 15:26:13 +0700

    Update Owner.java

--- Owner.java
+++ Owner.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.task_player;
-
-/**
- *
- * @author mdwivaaryaerlangga
- */
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
