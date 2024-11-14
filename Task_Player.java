1494864151f86e1629bd3577980a93f4f948d55e
Muhammad Dwiva Arya Erlangga <121459986+ary3141@users.noreply.github.com>
2024-11-14 15:16:03 +0700

    Add files via upload

---
+++ Task_Player.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.task_player;
+
+/**
+ *
+ * @author mdwivaaryaerlangga
+ */
+public class Task_Player {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
