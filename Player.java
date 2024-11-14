4f66eb7b95b4c82f90605556998cd050fa7ff0c0
Muhammad Dwiva Arya Erlangga <121459986+ary3141@users.noreply.github.com>
2024-11-14 15:26:27 +0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.task_player;
-
-/**
- *
- * @author mdwivaaryaerlangga
- */
 import java.util.ArrayList;
 
 public class Player extends User {
