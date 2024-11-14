4d6e2ad23a91d7f67c5a3b9de4d6c36d8402299e
Muhammad Dwiva Arya Erlangga <121459986+ary3141@users.noreply.github.com>
2024-11-14 15:27:18 +0700

    Update Task_Player.java

--- Task_Player.java
+++ Task_Player.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.task_player;
-
-/**
- *
- * @author mdwivaaryaerlangga
- */
 public class Task_Player {
 
     public static void main(String[] args) {
